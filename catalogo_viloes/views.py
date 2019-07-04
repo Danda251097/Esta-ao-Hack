692e28db9aa9463c1bb8f9a5b79386123ec9f2c1
Dandara <dandaraetec18@gmail.com>
2019-07-04 16:02:14 -0300

    baixar o heroku

--- catalogo_viloes/views.py
+++ catalogo_viloes/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from catalogo_viloes.models import Vilao
 
 # Create your views here.
 
 def mostrar_index(request):
-    return render(request, 'index.html')
+    viloes = Vilao.objects.all()
+    return render(request, 'index.html',{'viloes':viloes})
+
+def mostrar_vilas(request):
+    vilas=vilao.object.filter(categoria='mana')
+    return render (request),'manas.html'
